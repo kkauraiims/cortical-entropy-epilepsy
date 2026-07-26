0ba27faf37157ce0ef0a9e8046e01593f7c10c99
Kirandeep Kaur <89849204+kkauraiims@users.noreply.github.com>
2026-07-26 19:04:24 +0100

    Create 02_unpivot_seizfreq_table.sql

---
+++ scripts/mixed_effects_model/sql/02_unpivot_seizfreq_table.sql
@@ -0,0 +1,36 @@
+DROP VIEW IF EXISTS seizures_long;
+CREATE VIEW seizures_long AS
+
+WITH unpivoted AS (
+SELECT hour, 'lg1' as rodent_id, lg1 as sf
+FROM lg1_seizures
+
+UNION ALL 
+
+SELECT hour, 'lg2', lg2
+FROM lg1_seizures
+
+UNION ALL 
+
+SELECT hour, 'lg3', lg3
+FROM lg1_seizures
+
+UNION ALL 
+
+SELECT hour, 'lg4', lg4
+FROM lg1_seizures
+
+UNION ALL 
+
+SELECT hour, 'lg5', lg5
+FROM lg1_seizures
+
+)
+SELECT 
+hour, 
+CAST((hour - 1) / 24 AS INTEGER) + 1 AS recording_day,
+    (hour - 1) % 24 AS hour_of_day,
+    rodent_id,
+	sf 
+	FROM unpivoted; 
+  
