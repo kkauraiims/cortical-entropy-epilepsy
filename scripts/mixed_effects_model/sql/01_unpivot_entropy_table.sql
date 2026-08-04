0c66a1c48da8807435daf3147d8453a8a5999a56
Kirandeep Kaur <89849204+kkauraiims@users.noreply.github.com>
2026-08-04 15:33:14 +0100

    Update 01_unpivot_entropy_table.sql

--- scripts/mixed_effects_model/sql/01_unpivot_entropy_table.sql
+++ scripts/mixed_effects_model/sql/01_unpivot_entropy_table.sql
@@ -1,69 +1,48 @@
-/* Alter the SE table to include hours */
-ALTER TABLE sample_ent_week1
-ADD COLUMN hour INTEGER;
+DROP TABLE IF EXISTS entropy_long;
 
-UPDATE sample_ent_week1
-SET hour = rowid;
-
-SELECT *
-FROM sample_ent_week1
-LIMIT 10;
-
-/* Unpivot the entropy table */
-
-DROP VIEW IF EXISTS entropy_long;
-CREATE VIEW entropy_long AS
+CREATE TABLE entropy_long AS
 
 WITH unpivoted AS (
 
-    SELECT hour, 'cont1' AS rodent_id, 'Control' AS group_name,
-           cont1 AS sampen
-    FROM sample_ent_week1
+    SELECT hour, 'cont1' AS rodent_id,
+           'Control' AS group_name, cont1 AS sampen
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'cont2', 'Control', cont2
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'cont3', 'Control', cont3
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'cont4', 'Control', cont4
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'cont5', 'Control', cont5
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'lg1', 'LGI1', lg1
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'lg2', 'LGI1', lg2
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'lg3', 'LGI1', lg3
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'lg4', 'LGI1', lg4
-    FROM sample_ent_week1
+    FROM se_data_clean
 
     UNION ALL
-
     SELECT hour, 'lg5', 'LGI1', lg5
-    FROM sample_ent_week1
+    FROM se_data_clean
 )
 
 SELECT
@@ -75,8 +54,6 @@ SELECT
     sampen
 FROM unpivoted;
 
-
-/* check the n_entries */
 SELECT COUNT(*) AS total_rows
 FROM entropy_long;
 
